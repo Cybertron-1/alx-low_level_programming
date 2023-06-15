79f3cf1e1495351bffa7de43f206c76371f837fe
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-06-15 13:35:44 +0000

    hyuio

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,15 @@
+/*
+ * File: 101-quote.c
+ * Auth: kobby cyber
+ */
 
 #include <unistd.h>
+
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard output.
+ *                followed by a new line, to standard error.
  *
- * Return:  1.
+ * Return: Always 1.
  */
 int main(void)
 {
