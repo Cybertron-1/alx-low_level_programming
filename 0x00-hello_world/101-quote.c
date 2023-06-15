231a7756c57ddd8ead39bb97126a8ed7c2d33d8a
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-06-15 13:32:40 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 
 #include <unistd.h>
-
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard output.
+ *
+ * Return:  1.
+ */
 int main(void)
 {
 	write(2,
