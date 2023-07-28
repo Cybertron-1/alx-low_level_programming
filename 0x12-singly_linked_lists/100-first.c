886471c6352f47da0394108ad69f4a328f26a880
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-28 11:06:14 +0000

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,7 @@ void first(void) __attribute__ ((constructor));
 /**
  * first - prints a sentence before the main
  * function is executed void doesn't return a 0 
+ * ___attribute will always return 0 after code execution
  */
 void first(void)
 {
