91d8b87036e9491e5940e220b5b19aa1dd288565
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-28 11:07:21 +0000

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,7 +6,7 @@
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
- *
+ *stdlib imported 
  * Return: the address of the new element, or NULL if it fails
  *return a pointer location 
  */
