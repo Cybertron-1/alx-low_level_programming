4d17883c2fd3342483db583dd995bc5d8f4c921e
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-28 11:18:25 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,24 +6,16 @@
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed on terminal
- * print_list - prints all the elements of a linked list
- * int a = 0;
- * int b = 1;
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
+	if (h == NULL)
+		return 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		x++;
-	}
+	if (!h->str)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
 
-	return (x);
+	return 1 + print_list(h->next);
 }
