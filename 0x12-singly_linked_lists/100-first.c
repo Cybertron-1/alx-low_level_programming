3555c0c19b1d92e50d37fdc395e6b8e1f3bebb57
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-28 11:46:47 +0000

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,13 +7,13 @@ void first(void);
  */
 void pre_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
 int main(void)
 {
-    pre_main(); // Call the function manually before the main function
-    // Rest of the main function
-    return 0;
+	pre_main(); // Call the function manually before the main function
+	// Rest of the main function
+	return (0);
 }
