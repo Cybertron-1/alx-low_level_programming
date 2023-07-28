62fb6740219bb6fc381ef4388aff8328ce655235
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-28 11:45:33 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,12 @@
 size_t print_list(const list_t *h)
 {
 	if (h == NULL)
-		return 0;
+		return (0);
 
 	if (!h->str)
 		printf("[0] (nil)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
 
-	return 1 + print_list(h->next);
+	return (1 + print_list(h->next));
 }
