534a54ccc27e55566b508ad7db838035436e1a2b
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-31 23:50:45 +0000

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,15 +1,20 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop in a linked list.
- * @head: Linked list to search for.
+ * find_listint_loop - Finds the start node of the loop in a linked list.
+ * @head: Pointer to the first node of the list.
  *
- * Return: Address of the node where the loop starts, or NULL.
+ * Return: The address of the node where the loop starts, or NULL if there's no loop.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow = head;
-	listint_t *fast = head;
+	listint_t *slow, *fast;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	slow = head;
+	fast = head;
 
 	while (fast != NULL && fast->next != NULL)
 	{
@@ -18,15 +23,19 @@ listint_t *find_listint_loop(listint_t *head)
 
 		if (slow == fast)
 		{
-			listint_t *start = head;
-			while (start != slow)
+			/* Loop detected, reset slow to head and start from the beginning */
+			slow = head;
+
+			/* Move slow and fast at the same pace until they meet again */
+			while (slow != fast)
 			{
-				start = start->next;
 				slow = slow->next;
+				fast = fast->next;
 			}
-			return (start);
+
+			return (slow); /* Return the address of the loop start node */
 		}
 	}
 
-	return (NULL);
+	return (NULL); /* No loop found, return NULL */
 }
