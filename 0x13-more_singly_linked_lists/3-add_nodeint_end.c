dce6f35a3797406ab74ea21e95c02e775b3a2985
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-31 23:16:03 +0000

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,37 +1,37 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - Adds a node at the end of a linked list.
- * @head: Pointer to the first element in the list.
- * @n: Data to insert in the new element.
+ * add_nodeint_end - Adds a new node at the end of the list.
+ * @head: Pointer to the first node of the list.
+ * @n: Element (int) to add to the new node.
  *
- * Return: Address of the new element, or NULL if it fails.
+ * Return: Address of the new element or NULL if it fails.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	/* Create a new node and allocate memory for it */
-	listint_t *new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	listint_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	/* Initialize the new node with data and set the next pointer to NULL */
 	new_node->n = n;
 	new_node->next = NULL;
 
+	last_node = *head;
+
 	/* If the list is empty, set the new node as the head */
-	if (*head == NULL)
-	{
+	if (last_node == NULL)
 		*head = new_node;
-		return (new_node);
+	else
+	{
+		/* Traverse the list until the last node is reached */
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+
+		/* Attach the new node at the end of the list */
+		last_node->next = new_node;
 	}
-
-	/* Traverse the list until the last node is reached */
-	listint_t *current = *head;
-	while (current->next != NULL)
-		current = current->next;
-
-	/* Attach the new node at the end of the list */
-	current->next = new_node;
 
 	return (new_node);
 }
