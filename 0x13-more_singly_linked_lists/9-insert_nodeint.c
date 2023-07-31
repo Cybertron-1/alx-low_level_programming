83d0656608d8fd6049d39dad3bdc3f2c5e42337e
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-31 23:39:57 +0000

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,47 +2,47 @@
 #include <stdlib.h>
 
 /**
- * custom_insert_nodeint_at_index - Insert a new node at a given position.
- * @list: Address of the first node of the list.
- * @index: Position of the new node to be inserted in.
- * @data: Data of the new node.
+ * insert_nodeint_at_index - Inserts a new node at a given position.
+ * @head: Pointer to the first node of the list.
+ * @idx: Index of the list where the new node should be added.
+ * @n: Data of the new node.
  *
- * Return: Address of the new node.
- **/
-listint_t *custom_insert_nodeint_at_index(listint_t **list, unsigned int index, int data)
+ * Return: Address of the new node, or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *current;
-	unsigned int i = 0;
+	unsigned int i;
 
-	if (*list == NULL && index != 0)
+	if (head == NULL)
 		return (NULL);
 
-	if (index != 0)
-	{
-		current = *list;
-		while (i < index - 1 && current != NULL)
-		{
-			current = current->next;
-			i++;
-		}
-
-		if (current == NULL)
-			return (NULL);
-	}
-
+	/* Create the new node and set its data */
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
 
-	new_node->n = data;
-
-	if (index == 0)
+	if (idx == 0)
 	{
-		new_node->next = *list;
-		*list = new_node;
+		/* Insert at the beginning of the list */
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 
+	current = *head;
+	for (i = 0; current != NULL && i < idx - 1; i++)
+		current = current->next;
+
+	if (current == NULL)
+	{
+		/* Failed to find the index */
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Insert new_node after current */
 	new_node->next = current->next;
 	current->next = new_node;
 
