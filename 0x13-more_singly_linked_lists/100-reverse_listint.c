e64520f7880928ef66fcdca723527fe3da251bfc
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-01 12:16:35 +0000

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,33 +1,26 @@
 #include "lists.h"
 
 /**
- * reverse_listint_recursive - Helper function to reverse a linked list recursively.
- * @current: Current node to reverse.
- * @prev: Previous node (NULL initially).
- *
- * Return: A pointer to the first node of the reversed list.
- */
-listint_t *reverse_listint_recursive(listint_t *current, listint_t *prev)
-{
-    listint_t *next = current->next;
-    current->next = prev;
-
-    if (next == NULL)
-        return current;
-
-    return reverse_listint_recursive(next, current);
-}
-
-/**
  * reverse_listint - Reverses a listint_t linked list.
- * @head: Pointer to the head of the list.
+ * @head: Pointer to the head of the linked list.
  *
  * Return: A pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    if (head == NULL || *head == NULL)
-        return NULL;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-    return reverse_listint_recursive(*head, NULL);
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+
+	return *head;
 }
