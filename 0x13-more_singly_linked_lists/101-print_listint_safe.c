e6d9a6d20e2e020fcbeb96fc8c787a96505864f1
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-01 12:28:25 +0000

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,35 @@
 #include "lists.h"
+
 /**
  * print_listint_safe - function that prints a linked list with a loop safely.
  * @head: pointer to the 1st node of the linked list
- * Return: new_node
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp_n = NULL;
-	const listint_t *l_n = NULL;
-	size_t counter = 0;
-	size_t new_n;
+	const listint_t *fast, *slow;
+	size_t nnodes = 0;
 
-	tmp_n = head;
-	while (tmp_n)
+	slow = fast = head;
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
-		counter++;
-		tmp_n = tmp_n->next;
-		l_n = head;
-		new_n = 0;
-		while (new_n < counter)
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
 		{
-			if (tmp_n == l_n)
-			{
-				printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
-				return (counter);
-			}
-			l_n = l_n->next;
-			new_n++;
+			printf("Loop detected, stopping printing...\n");
+			return (nnodes);
 		}
-		if (!head)
-			exit(98);
 	}
-	return (counter);
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
+	}
+
+	return (nnodes);
 }
