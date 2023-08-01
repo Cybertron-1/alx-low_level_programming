0fe3c0280d0e95f274f2bc6d28be8700ae51931d
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-01 12:25:00 +0000

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_listp(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
  * print_listint_safe - prints a linked list.
  * @head: head of a list.
  *
@@ -32,39 +9,28 @@ void free_listp(listp_t **head)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nnodes = 0;
-	listp_t *visited = NULL;
-	listp_t *new_node = NULL;
-	listp_t *checker = NULL;
+	listint_t *fast, *slow;
+
+	fast = slow = (listint_t *)head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			printf("Loop detected, stopping printing...\n");
+			return (nnodes);
+		}
+	}
 
 	while (head != NULL)
 	{
-		new_node = malloc(sizeof(listp_t));
-
-		if (new_node == NULL)
-			exit(98);
-
-		new_node->p = (void *)head;
-		new_node->next = visited;
-		visited = new_node;
-
-		checker = visited;
-
-		while (checker->next != NULL)
-		{
-			checker = checker->next;
-			if (head == checker->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&visited);
-				return (nnodes);
-			}
-		}
-
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 		nnodes++;
 	}
 
-	free_listp(&visited);
 	return (nnodes);
 }
