ffab296cd543572e813cc5f81fe07b5382c1a219
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-03 14:09:06 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,54 +1,70 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: string that contains 0 and 1 characters
+ * _atoi - converts chars to integer
  *
- * Return: the number converted or 0 if @b contains a character that is not 0 or 1,
- *         or when @b is null
- */
-unsigned int binary_to_uint(const char *b)
-{
-    if (b == NULL)
-        return (0);
-
-    unsigned int result = 0;
-
-    while (*b)
-    {
-        if (*b != '0' && *b != '1')
-            return (0);
-
-        result = (result * 2) + (*b - '0');
-        b++;
-    }
-
-    return (result);
-}
-
-/**
- * _atoi - converts a char to an unsigned int
  * @c: char to convert
  *
  * Return: converted integer
- */
+*/
 unsigned int _atoi(char c)
 {
-    return ((unsigned int)(c - '0'));
+	return ((unsigned int) c - '0');
 }
 
 /**
  * _strlen - gets the length of a string
+ *
  * @str: string input
  *
  * Return: string length
- */
+*/
 unsigned int _strlen(const char *str)
 {
-    unsigned int length = 0;
+	unsigned int index = 0;
 
-    while (str[length] != '\0')
-        length++;
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
 
-    return (length);
+/**
+ * binary_to_uint - a function that converts a binary number
+ *                  to an unsigned int
+ *
+ * @b: string that contains 0 and 1 characters
+ *
+ * Return: the number converted or 0
+ *         if @b contains a character
+ *         that is not 0 or 1 or when
+ *         @b is null
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	int index;
+	unsigned int result = 0, base2 = 1,  num = 0;
+
+	/*if b is NULL return 0*/
+	if (b == NULL)
+		return (0);
+
+	/*iterate through string*/
+	for (index = _strlen(b) - 1; index >= 0; index--)
+	{
+		num = _atoi(b[index]); /*convert char to number*/
+
+		/*if number is not 0 or 1 return 0*/
+		if (num != 0 && num != 1)
+			return (0);
+
+		result += num * base2; /*enable debug to see it in action*/
+		base2 *= 2;
+
+		#ifdef DEBUG /*print debug statements*/
+		printf("Result is %u.\n", result);
+		#endif
+	}
+
+	return (result);
+
 }
