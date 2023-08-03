3f2e3eed96d9dbf269574279009688a546598c25
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-03 14:12:31 +0000

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,97 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_binary - Print the binary representation of a number.
- * @n: Decimal number to print as binary.
- */
+ * debug_1 - print debug statements
+ *
+ * @n: value of n
+ * @length: length of n
+ * @mask: mask of n
+ *
+ * Return: nothing
+*/
+void debug_1(unsigned long int n, int length, unsigned long int mask)
+{
+	printf("Length of %lu (n) is %i ", n, (length + 1));
+	printf("and mask (original value 1) ");
+	printf("is %lu based ", mask);
+	printf("on [length of n - 1] == %i.\n\n", length);
+}
+
+/**
+ * debug_2 - print debug statements
+ *
+ * @n: value of n
+ * @mask: mask of n
+ *
+ * Return: nothing
+*/
+void debug_2(unsigned long int n, unsigned long int mask)
+{
+	printf("Value of n is %lu, ", n);
+	printf("value of mask is %lu and ", mask);
+	printf("value of [n & mask] is %lu.\n\n", (n & mask));
+}
+
+/**
+ * debug_3 - print debug statements
+ *
+ * @mask: mask of value n
+ *
+ * Return: nothing
+*/
+void debug_3(unsigned long int mask)
+{
+	printf("\nValue of mask is %lu after right shifting by one.\n\n", mask);
+}
+
+/**
+ * _length - find the length of @n
+ *
+ * @n: value to find its length
+ *
+ * Return: length
+*/
+int _length(unsigned long int n)
+{
+	int length = 0;
+
+	while (n > 0)
+	{
+
+		length++;
+		n >>= 1; /*shift n to the right by 1*/
+
+	}
+
+	length--;
+
+	return (length);
+}
+
+/**
+ * print_binary - a function that converts a decimal to binary
+ *
+ * @n: decimal number to convert
+ *
+ * Return: nothing
+*/
 void print_binary(unsigned long int n)
 {
+	int length;
 	unsigned long int mask = 1;
-	int num_bits = sizeof(unsigned long int) * 8;
-	int bit;
 
-	for (bit = num_bits - 1; bit >= 0; bit--)
+	length = _length(n);
+
+	if (length > 0) /*create mask based on length of number*/
+		mask <<= length; /*shift mask to the left by length*/
+
+	while (mask > 0)
 	{
-		if ((n & (mask << bit)) != 0)
-			putchar('1');
-		else
-			putchar('0');
+		if (n & mask) /*if n & mask == 1 print 1*/
+			_putchar('1');
+		else /*else if n & mask == 0 print 0*/
+			_putchar('0');
+
+		mask >>= 1; /*shift mask to the right by 1*/
 	}
-	putchar('\n');
 }
