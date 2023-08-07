7fa10b009b3a906b27419b700bb2774e1fac5691
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-07 16:57:03 +0000

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int _file, _write, text_len = 0;
+	int _file, _write, text_length = 0;
 
 	if (!filename)
 		return (-1);
@@ -21,10 +21,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[text_len])
+		while (text_content[text_length])
 			text_len++;
 
-		_write = write(_file, text_content, text_len);
+		_write = write(_file, text_content, text_length);
 		if (_write == -1)
 		{
 			close(_file);
