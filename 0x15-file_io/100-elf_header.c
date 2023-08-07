fe52d3f559c34368247074b92bd075855422d37d
Cybertron-1 <kobbycyber1@gmail.com>
2023-08-07 08:22:32 -0700

    elf header update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,67 +1,109 @@
 #include "main.h"
 
 /**
- * main - displays the contents of an ELF header
- * @argc: argument count
- * @argv: argument vector
- * Description: ./elf_header elf_file
- * Return: 0 on success, otherwise exit with error codes
+ * print_elf_header - Prints the information contained in the ELF header.
+ * @header: Pointer to the ELF header structure.
+ */
+void print_elf_header(Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header->e_ident[i]);
+	printf("\n");
+
+	printf("  Class:                             ");
+	switch (header->e_ident[EI_CLASS])
+	{
+		case ELFCLASSNONE: printf("This class is invalid\n"); break;
+		case ELFCLASS32: printf("ELF32\n"); break;
+		case ELFCLASS64: printf("ELF64\n"); break;
+		default: printf("<unknown: %x>\n", header->e_ident[EI_CLASS]);
+	}
+
+	printf("  Data:                              ");
+	switch (header->e_ident[EI_DATA])
+	{
+		case ELFDATANONE: printf("Unknown data format\n"); break;
+		case ELFDATA2LSB: printf("2's complement, little endian\n"); break;
+		case ELFDATA2MSB: printf("2's complement, big endian\n"); break;
+		default: printf("<unknown: %x>\n", header->e_ident[EI_DATA]);
+	}
+
+	printf("  Version:                           ");
+	if (header->e_ident[EI_VERSION] == EV_CURRENT)
+		printf("%i (current)\n", EV_CURRENT);
+	else
+		printf("%i\n", header->e_ident[EI_VERSION]);
+
+	printf("  OS/ABI:                            ");
+	switch (header->e_ident[EI_OSABI])
+	{
+		case ELFOSABI_SYSV: printf("UNIX - System V\n"); break;
+		case ELFOSABI_HPUX: printf("UNIX - HP-UX\n"); break;
+		case ELFOSABI_NETBSD: printf("UNIX - NetBSD\n"); break;
+		case ELFOSABI_LINUX: printf("UNIX - Linux\n"); break;
+		case ELFOSABI_SOLARIS: printf("UNIX - Solaris\n"); break;
+		case ELFOSABI_IRIX: printf("UNIX - IRIX\n"); break;
+		case ELFOSABI_FREEBSD: printf("UNIX - FreeBSD\n"); break;
+		case ELFOSABI_TRU64: printf("UNIX - TRU64\n"); break;
+		case ELFOSABI_ARM: printf("ARM\n"); break;
+		case ELFOSABI_STANDALONE: printf("Standalone App\n"); break;
+		default: printf("<unknown: %x>\n", header->e_ident[EI_OSABI]);
+	}
+
+	printf("  ABI Version:                       %i\n", header->e_ident[EI_ABIVERSION]);
+
+	printf("  Type:                              ");
+	switch (header->e_type)
+	{
+		case ET_NONE: printf("NONE (Unknown type)\n"); break;
+		case ET_REL: printf("REL (Relocatable file)\n"); break;
+		case ET_EXEC: printf("EXEC (Executable file)\n"); break;
+		case ET_DYN: printf("DYN (Shared object file)\n"); break;
+		case ET_CORE: printf("CORE (Core file)\n"); break;
+		default: printf("<unknown: %x>\n", header->e_type);
+	}
+
+	printf("  Entry point address:               %#lx\n", header->e_entry);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: 0 on success, 1 on failure.
  */
 int main(int argc, char *argv[])
 {
-	int fd, read_bytes, close_ret;
-	Elf64_Ehdr *header;
+	int fd, read_l;
+	Elf64_Ehdr header;
 
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s elf_file\n", argv[0]);
-		exit(98);
-	}
-
-	header = malloc(sizeof(Elf64_Ehdr));
-	if (header == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
-		exit(98);
+		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
+		return (EXIT_FAILURE);
 	}
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
-		free(header);
-		exit(98);
+		return (EXIT_FAILURE);
 	}
 
-	read_bytes = read(fd, header, sizeof(Elf64_Ehdr));
-	if (read_bytes == -1)
+	read_l = read(fd, &header, sizeof(header));
+	if (read_l != sizeof(header))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		free(header);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd);
-		exit(98);
+		return (EXIT_FAILURE);
 	}
 
-	elf_validation(header->e_ident);
-	print_magic(header->e_ident);
-	print_class(header->e_ident);
-	print_data(header->e_ident);
-	print_version(header->e_ident);
-	print_osabi(header->e_ident);
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
+	print_elf_header(&header);
 
-	free(header);
-
-	close_ret = close(fd);
-	if (close_ret == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-		exit(98);
-	}
-
-	return (0);
+	close(fd);
+	return (EXIT_SUCCESS);
 }
-
-/* Remaining functions (elf_validation, print_magic, print_class, etc.) remain the same as in the original code. */
