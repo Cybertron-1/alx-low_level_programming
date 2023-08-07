486c198a754902f9d7a0e037224f3b445498b2b0
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-07 15:04:50 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
- * main - copies the content of a file to another file executable
+ * main - copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 if program was able to copy
+ * Return: 0 if success
  */
 
 int main(int argc, char *argv[])
 {
-	int file1;
-	int file2;
-	int _readfile;
-	int datahold1; 
-	int datahold2;
+	int file1, file2, _readdata, d1, d2;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -26,25 +22,25 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((_readfile = read(file1, buffer, 1024)) > 0)
+	while ((_readdata = read(file1, buffer, 1024)) > 0)
 	{
-		if (file2 < 0 || (write(file2, buffer, _readfile) != _readfile))
+		if (file2 < 0 || (write(file2, buffer, _readdata) != _readdata))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
-	if (_readfile < 0)
+	if (_readdata < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	datahold1 = close(file1);
-	if (datahold1 < 0)
+	d1 = close(file1);
+	if (d1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1), exit(100);
 	}
-	datahold2 = close(file2);
-	if (datahold2 < 0)
+	d2 = close(file2);
+	if (d2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2), exit(100);
 	}
