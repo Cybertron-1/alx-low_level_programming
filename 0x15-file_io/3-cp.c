6a50c5d1b3ba9dc875e61d318b224641d96d7f09
Cybertron-1 <kobbycyber1@gmail.com>
2023-08-07 07:32:07 -0700

    cp copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,56 +9,50 @@
  */
 int main(int argc, char *argv[])
 {
-	int src_fd, dest_fd, bytes_read, close_status1, close_status2;
+	int file_from, file_to, bytes_read, close_status1, close_status2;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
 
-	src_fd = open(argv[1], O_RDONLY);
-	if (src_fd < 0)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		exit(98);
 	}
 
-	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (dest_fd < 0)
+	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(src_fd);
-		return (99);
-	}
-
-	while ((bytes_read = read(src_fd, buffer, 1024)) > 0)
-	{
-		if (write(dest_fd, buffer, bytes_read) != bytes_read)
+		if (file_to < 0 || (write(file_to, buffer, bytes_read) != bytes_read))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(src_fd);
-			close(dest_fd);
-			return (99);
+			exit(99);
 		}
 	}
 
 	if (bytes_read < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(src_fd);
-		close(dest_fd);
-		return (98);
+		exit(98);
 	}
 
-	close_status1 = close(src_fd);
-	close_status2 = close(dest_fd);
-
-	if (close_status1 < 0 || close_status2 < 0)
+	close_status1 = close(file_from);
+	if (close_status1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
-		return (100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
+		exit(100);
+	}
+
+	close_status2 = close(file_to);
+	if (close_status2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
+		exit(100);
 	}
 
 	return (0);
