787c8ac8eb6696eff9babdfa0c5f54c1f8e9a9b2
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-07 14:40:45 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * main - Copies the content of a file to another file
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: 0 if success, 97, 98, 99, or 100 on failure
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, bytes_read, close_status1, close_status2;
+	int file1, file2, read_bytes, c1, c2;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -18,40 +17,40 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from < 0)
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
+	file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	while ((read_bytes = read(file1, buffer, 1024)) > 0)
 	{
-		if (file_to < 0 || (write(file_to, buffer, bytes_read) != bytes_read))
+		if (file2 < 0 || (write(file2, buffer, read_bytes) != read_bytes))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 
-	if (bytes_read < 0)
+	if (read_bytes < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	close_status1 = close(file_from);
-	if (close_status1 < 0)
+	c1 = close(file1);
+	if (c1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1);
 		exit(100);
 	}
 
-	close_status2 = close(file_to);
-	if (close_status2 < 0)
+	c2 = close(file2);
+	if (c2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2);
 		exit(100);
 	}
 
