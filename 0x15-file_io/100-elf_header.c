e1ac18ef8c8ffbbccc76ff7b441c6b6816ac2389
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-08-07 15:38:42 +0000

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,79 +1,114 @@
 #include "main.h"
 
 /**
- * Reads the ELF header from the specified file and prints its contents.
+ * main - displays the contents of an ELF header
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: ./elf_header elf_file
+ * Return: 0 - success, 98 - error
  */
 int main(int argc, char *argv[])
 {
-    int fd, read_l;
-    Elf64_Ehdr header;
+    int ret;
 
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s elf_file\n", argv[0]);
-        exit(EXIT_FAILURE);
+        dprintf(STDERR_FILENO, "Usage: %s elf_file\n", argv[0]);
+        return (98);
     }
 
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    read_l = read(fd, &header, sizeof(Elf64_Ehdr));
-    if (read_l != sizeof(Elf64_Ehdr))
-    {
-        fprintf(stderr, "Error: Can't read file %s\n", argv[1]);
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    close(fd);
-
-    elf_validation(header.e_ident);
-    print_magic(header.e_ident);
-    print_class(header.e_ident);
-    print_data(header.e_ident);
-    print_version(header.e_ident);
-    print_osabi(header.e_ident);
-    printf("  ABI Version:                       ");
-    printf("%i\n", header.e_ident[EI_ABIVERSION]);
-    print_type(header.e_type, header.e_ident);
-    print_entry(header.e_entry, header.e_ident);
-
-    return 0;
+    ret = print_elf_header_info(argv[1]);
+    return (ret);
 }
 
 /**
- * Validates whether a file is in ELF format.
+ * print_elf_header_info - prints information from the ELF header
+ * @filename: the name of the ELF file
+ *
+ * Return: 0 - success, 98 - error
+ */
+int print_elf_header_info(const char *filename)
+{
+    int fd, read_l;
+    Elf64_Ehdr header;
+    unsigned char e_ident[EI_NIDENT];
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
+        return (98);
+    }
+
+    read_l = read(fd, &header, sizeof(Elf64_Ehdr));
+    if (read_l == -1)
+    {
+        dprintf(STDERR_FILENO, "Can't read file %s\n", filename);
+        close(fd);
+        return (98);
+    }
+
+    memcpy(e_ident, header.e_ident, EI_NIDENT);
+    close(fd);
+
+    elf_validation(e_ident);
+    print_magic(e_ident);
+    print_class(e_ident);
+    print_data(e_ident);
+    print_version(e_ident);
+    print_osabi(e_ident);
+    printf("  ABI Version:                       %i\n", e_ident[EI_ABIVERSION]);
+    
+    unsigned int e_type = header.e_type;
+    if (e_ident[EI_DATA] == ELFDATA2MSB)
+    {
+        e_type = (e_type >> 8) & 0xFF;
+    }
+    print_type(e_type, e_ident);
+
+    unsigned int e_entry = header.e_entry;
+    if (e_ident[EI_DATA] == ELFDATA2MSB)
+    {
+        e_entry = ((e_entry >> 24) & 0xFF) | ((e_entry >> 8) & 0xFF00) | ((e_entry << 8) & 0xFF0000) | ((e_entry << 24) & 0xFF000000);
+    }
+    print_entry(e_entry, e_ident);
+
+    return (0);
+}
+
+/**
+ * elf_validation - validates whether a file is in ELF format
+ * @e_ident: file description
  */
 void elf_validation(unsigned char *e_ident)
 {
     if (e_ident[0] != 0x7f || e_ident[1] != 'E' || e_ident[2] != 'L' || e_ident[3] != 'F')
     {
-        fprintf(stderr, "Error: File not an ELF\n");
-        exit(EXIT_FAILURE);
+        dprintf(STDERR_FILENO, "Error: File not an ELF\n");
+        exit(98);
     }
 }
 
 /**
- * Prints an ELF's magic number.
+ * print_magic - prints an ELF's magic number
+ * @e_ident: file description
  */
 void print_magic(unsigned char *e_ident)
 {
     int i;
 
     printf("  Magic:   ");
-    for (i = 0; i < EI_NIDENT - 1; i++)
+    for (i = 0; i < EI_NIDENT; i++)
     {
         printf("%02x ", e_ident[i]);
     }
-    printf("%02x\n", e_ident[i]);
+    printf("\n");
 }
 
 /**
- * Prints an ELF's class.
+ * print_class - prints an ELF's class
+ * @e_ident: pointer to string
  */
 void print_class(unsigned char *e_ident)
 {
@@ -95,7 +130,8 @@ void print_class(unsigned char *e_ident)
 }
 
 /**
- * Prints an ELF's data.
+ * print_data - prints an ELF's data
+ * @e_ident: pointer to string
  */
 void print_data(unsigned char *e_ident)
 {
@@ -117,7 +153,8 @@ void print_data(unsigned char *e_ident)
 }
 
 /**
- * Prints an ELF's version.
+ * print_version - prints an ELF's version
+ * @e_ident: string pointer
  */
 void print_version(unsigned char *e_ident)
 {
@@ -129,7 +166,8 @@ void print_version(unsigned char *e_ident)
 }
 
 /**
- * Prints an ELF's OS/ABI.
+ * print_osabi - prints an ELF's OS/ABI
+ * @e_ident: string pointer
  */
 void print_osabi(unsigned char *e_ident)
 {
@@ -172,12 +210,16 @@ void print_osabi(unsigned char *e_ident)
 }
 
 /**
- * Prints an ELF's type.
+ * print_type - prints an ELF's type
+ * @e_type: status int
+ * @e_ident: pointer to string
  */
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
     if (e_ident[EI_DATA] == ELFDATA2MSB)
+    {
         e_type = e_type >> 8;
+    }
 
     printf("  Type:                              ");
     switch (e_type)
@@ -203,24 +245,16 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 }
 
 /**
- * Prints an ELF's entry point address.
+ * print_entry - prints an ELF's entry point address
+ * @e_entry: e_entry address
+ * @e_ident: pointer to char string
  */
 void print_entry(unsigned int e_entry, unsigned char *e_ident)
 {
     if (e_ident[EI_DATA] == ELFDATA2MSB)
-        e_entry = lit_to_big_endian(e_entry);
+    {
+        e_entry = ((e_entry >> 24) & 0xFF) | ((e_entry >> 8) & 0xFF00) | ((e_entry << 8) & 0xFF0000) | ((e_entry << 24) & 0xFF000000);
+    }
 
-    printf("  Entry point address:               ");
-    printf("%#x\n", (unsigned int)e_entry);
-}
-
-/**
- * Converts a value from little endian to big endian.
- */
-unsigned int lit_to_big_endian(unsigned int x)
-{
-    return (((x >> 24) & 0x000000ff) |
-            ((x >> 8) & 0x0000ff00) |
-            ((x << 8) & 0x00ff0000) |
-            ((x << 24) & 0xff000000));
+    printf("  Entry point address:               %#x\n", e_entry);
 }
